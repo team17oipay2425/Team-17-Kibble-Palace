08e120d88172301376510924ded70c17e3995688
Garygohjr <garygohjr@gmail.com>
2025-08-07 14:00:48 +0100

    feat: responsive site, day 12, 13 dailies and group photo about us

--- nav.js
+++ nav.js
@@ -1,5 +1,12 @@
 fetch('nav.html')
-.then(response => response.text())
-.then(data => {
-document.getElementById('navbar').innerHTML = data;
-});
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById('navbar').innerHTML = data;
+
+    const hamburger = document.querySelector('.hamburger');
+    const navMenu = document.querySelector('.main-nav');
+
+    hamburger.addEventListener('click', () => {
+      navMenu.classList.toggle('active');
+    });
+  });
